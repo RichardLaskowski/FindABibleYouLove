d92aef86b42342aae3b26eaac34b8e1bbd4a32f4
Richard Laskowski <richard.l.laskowski@gmail.com>
2023-05-20 08:52:13 -0500

    GetAllBibleFeatures unit tests created.

---
+++ tests/API.Unit.Tests/BibleFeature/GetAllBibleFeaturesTests.cs
@@ -0,0 +1,45 @@
+﻿using API.Endpoints.Bible;
+using API.Unit.Tests.Fakes;
+
+using FindABibleYouLove.Contracts.Bible;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace API.Unit.Tests.BibleFeature;
+
+public class GetAllBibleFeaturesTests
+{
+    [Fact]
+    public async Task GetAllBibleFeatures_ReturnsNotFound_WhenNoFeaturesExists()
+    {
+        // Arrange
+        BibleFeatureEndpoint sut = new();
+        BibleFeatureServiceFake bibleFeatureServiceFake = new BibleFeatureServiceFake(new List<BibleFeatureContract>());
+
+        // Act
+
+        IResult result = await sut.GetAllBibleFeaturesAsync(bibleFeatureServiceFake);
+
+        // Assert
+        Assert.IsType<NotFound>(result);
+    }
+
+    [Fact]
+    public async Task GetAllBibleFeatures_ReturnsOk_WhenFeaturesExists()
+    {
+        // Arrange
+        BibleFeatureEndpoint sut = new();
+        BibleFeatureServiceFake bibleFeatureServiceFake = new(new List<BibleFeatureContract>()
+        {
+            new BibleFeatureContract(Guid.NewGuid().ToString(), "Feature"),
+        });
+
+        // Act
+
+        IResult result = await sut.GetAllBibleFeaturesAsync(bibleFeatureServiceFake);
+
+        // Assert
+        Assert.IsType<Ok<IEnumerable<BibleFeatureContract>>>(result);
+    }
+}
